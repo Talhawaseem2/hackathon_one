380148617674ef9cc2e93160108dead0c71b3b66
Talhawaseem2 <114868886+Talhawaseem2@users.noreply.github.com>
2023-08-19 03:15:00 +0500

    error solved and some editing

--- src/components/views/ProductDetail/index.tsx
+++ src/components/views/ProductDetail/index.tsx
@@ -8,6 +8,8 @@ import { client } from "../../../../sanity/lib/client";
 import { SanityClient } from "sanity";
 import Image from "next/image";
 import ImageUrlBuilder from "@sanity/image-url";
+import { Button } from "@/components/ui/button";
+import { ShoppingCart } from "lucide-react";
 
 const builder: any = ImageUrlBuilder(client);
 
@@ -18,9 +20,22 @@ function urlFor(source: any) {
 const ProductDetail: FC<{ item: oneProductType }> = ({ item }) => {
   const [ImageForPreviewOfSelected, setImageForPreviewOfSelected] =
     useState<string>(item.image[0]._key);
-  console.log("_key : ", ImageForPreviewOfSelected);
+  const [quantity, setQuantity] = useState(1);
+
+  function incrementTheQuantity () {
+    setQuantity(quantity + 1)
+  }
+  ;
+  function decrementTheQuantity () {
+    if(quantity === 0){
+
+    }else{
+        setQuantity(quantity - 1)
+      }
+  };
+  
   return (
-    <div>
+    <div className="flex flex-col lg:flex-row justify-center items-center">
       {/* {left} */}
       <div className="flex gap-x-4 md:gap-x-8">
         {/* left */}
@@ -47,7 +62,7 @@ const ProductDetail: FC<{ item: oneProductType }> = ({ item }) => {
             if (subItem._key === ImageForPreviewOfSelected) {
               return (
                 <Image
-                    key={index}
+                  key={index}
                   width={1000}
                   height={1000}
                   alt={subItem.alt}
@@ -59,22 +74,51 @@ const ProductDetail: FC<{ item: oneProductType }> = ({ item }) => {
         </div>
       </div>
       {/* right */}
-      <div>
+      <div className="p-6 space-y-8">
         <div>
-          <h1>{item.productName}</h1>
-          <p>{item.productTypes[1]}</p>
+          <h1 className="text-3xl text-gray-700 font- font-medium">
+            {item.productName}
+          </h1>
+          <p className="text-pink-600 text-xl font-medium">
+            {item.productTypes[1]}
+          </p>
         </div>
-        <div>
-          <p>Select Size</p>
+        <div className="space-y-2">
+          <p className="text-lg font-semibold text-gray-700">Select Size</p>
+          <div className="flex gap-2 text-pink-600">
+            {item.size.map((subItem: string, index: number) => (
+              <div
+                key={index}
+                className="hover:shadow-xl font-semibold cursor-pointer rounded-full bg-gray-100 w-12 h-12 flex justify-center items-center"
+              >
+                {subItem}
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="flex gap-4">
-          {
-              item.size.map((subItem : string , index : number) => (
-              <div key={index} className="rounded-full bg-gray-100 w-12 h-12 flex justify-center items-center">{subItem}</div>
-            ))
-          }
+        <div className="flex space-x-7">
+          <p className="font-semibold text-xl text-gray-800">Quantity:</p>
+          <div className="flex gap-2 items-center text-lg">
+            <div 
+            onClick={decrementTheQuantity}
+            className="select-none cursor-pointer flex justify-center items-center w-9 h-9 rounded-full bg-gray-100">
+              -
+            </div>
+            <p>{quantity}</p>
+            <div
+              onClick={incrementTheQuantity}
+             className="select-none cursor-pointer flex justify-center items-center w-9 h-9 rounded-full border border-gray-800">
+              +
+            </div>
+          </div>
         </div>
-        <div></div>
+        <div className="flex gap-x-8 items-center">
+          <Button className='px-6 h-12 text-white text-lg bg-black mt-5  '>
+                    <ShoppingCart className="mr-2 h-6 w-6" />
+                    Add to Cart
+                </Button>
+                <p className="text-2xl font-semibold ">RS:{item.price}</p>
+          </div>
       </div>
     </div>
   );
